f019f80b212a42e2e02437231fece758501b6362
shreenivas-nayakawadi <shreenivasofficial@gmail.com>
2025-02-27 17:51:24 +0530

    frontend setup with vite/react and tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home";
+
+function App() {
+      return (
+            <div>
+                  <Routes>
+                        <Route path="/" element={<Home />} />
+                  </Routes>
+            </div>
+      );
+}
+
+export default App;
